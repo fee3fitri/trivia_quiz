3cec1dc6e31f018764b9f16cb10acccf416a08a7
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-03-12 14:27:41 -0700

    Change params into parameters object

--- trivia_quiz/main.py
+++ trivia_quiz/main.py
@@ -3,7 +3,13 @@ from question_model import Question
 from quiz_brain import QuizBrain
 import requests
 
-response = requests.get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean")
+parameters = {
+    "amount" : 10, 
+    "difficulty": "easy", 
+    "type": "boolean",
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 question_data = response.json()['results']
 
